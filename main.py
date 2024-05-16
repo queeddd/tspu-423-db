257e784dc52e84a9e3cb4d9f108574e00f75bcaf
queeddd <125481637+queeddd@users.noreply.github.com>
2024-05-16 16:58:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 import sqlite3
 
-# Создаем подключение к базе данных (файл my_database.db будет создан)
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Создаем таблицу Пользователи
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Пользователи (
-id INT(11) NOT NULL AUTO_INCREMENT,
-Фамилия VARCHAR(60) NOT NULL,
-Имя VARCHAR(60) NOT NULL,
-Отчество VARCHAR(60), 
-Дата рождения DATE NOT NULL,
-Электронная почта VARCHAR(50) NOT NULL,
-Телефон СHAR(20),
-Форма обучения ENUM("очная", "заочная", "очно-заочная") NOT NULL,
-Статус TINYINT(1) NOT NULL DEFAULT = 1,
-Пароль VARCHAR(32) NOT NULL,
-ID_группы INT(11) NOT NULL
-)
-''')
+    CREATE TABLE IF NOT EXISTS faculty (
+        faculty_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        faculty_name VARCHAR(100) NOT NULL                       
+    )''')
 
-# Создаем таблицу Факультет
+
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Факультет (
-id INT(11) NOT NULL AUTO_INCREMENT,
-Название VARCHAR(100) NOT NULL
-)
-''')
+    CREATE TABLE IF NOT EXISTS groups (
+        group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        group_name VARCHAR(100) NOT NULL,
+        faculty_id INTEGER(11) NOT NULL,
+        FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_id)                                  
+    )''')
 
-# Создаем таблицу Группа
+
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Группа (
-id INT(11) NOT NULL AUTO_INCREMENT,
-Название VARCHAR(100) NOT NULL,
-ID_факультета INT(11) NOT NULL 
-)
-''')
-
-# Сохраняем изменения и закрываем соединение
+    CREATE TABLE IF NOT EXISTS user (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        last_name VARCHAR(60) NOT NULL,
+        first_name VARCHAR(30) NOT NULL,
+        fathers_name VARCHAR(30),
+        birth_date DATE NOT NULL,
+        Email VARCHAR(50) NOT NULL,
+        phone_number CHAR(20),
+        form_of_education VARCHAR(15) CHECK(form_of_education in ("очная", "заочная", "очно-заочная")) NOT NULL,
+        status TINYINT(1) DEFAULT 1 NOT NULL,
+        pass VARCHAR(32) NOT NULL,
+        group_id INTEGER(11) NOT NULL,
+        FOREIGN KEY (group_id) REFERENCES groups(group_id)
+    )''')
 connection.commit()
 connection.close()
