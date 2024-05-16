10935fda087d0af7e6616d5d4d9404b0563f50a9
queeddd <125481637+queeddd@users.noreply.github.com>
2024-05-16 17:04:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS faculty (
-        faculty_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        faculty_name VARCHAR(100) NOT NULL                       
+    CREATE TABLE IF NOT EXISTS faculties (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) NOT NULL                       
     )''')
 
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS groups (
-        group_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        group_name VARCHAR(100) NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) NOT NULL,
         faculty_id INTEGER(11) NOT NULL,
-        FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_id)                                  
+        FOREIGN KEY (faculty_id) REFERENCES faculties(id)                                  
     )''')
 
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS user (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         last_name VARCHAR(60) NOT NULL,
         first_name VARCHAR(30) NOT NULL,
         fathers_name VARCHAR(30),
         birth_date DATE NOT NULL,
-        Email VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
         phone_number CHAR(20),
         form_of_education VARCHAR(15) CHECK(form_of_education in ("очная", "заочная", "очно-заочная")) NOT NULL,
         status TINYINT(1) DEFAULT 1 NOT NULL,
-        pass VARCHAR(32) NOT NULL,
+        password VARCHAR(32) NOT NULL,
         group_id INTEGER(11) NOT NULL,
-        FOREIGN KEY (group_id) REFERENCES groups(group_id)
+        FOREIGN KEY (group_id) REFERENCES groups(id)
     )''')
 connection.commit()
 connection.close()
