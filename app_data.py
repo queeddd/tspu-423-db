77a1d9a86d5e8a34962a9c7af86cd3c383734590
queeddd <125481637+queeddd@users.noreply.github.com>
2024-05-30 09:30:24 +0700

    Update app_data.py

--- app_data.py
+++ app_data.py
@@ -5,75 +5,74 @@ cursor = connection.cursor()
 
 def add_faculties():
     cursor.execute('''
-    INSERT INTO faculties (name) VALUES 
-        ("ФМФ"),
-        ("ТЭФ"),
-        ("ИИЯМС")                      
-    ''')
+        INSERT INTO faculties (name) VALUES 
+            ("ФМФ"),
+            ("ИЯМС"),
+            ("ТЭФ")
+        ''')
 
 def truncate_faculties():
     cursor.execute('''
-        DELETE FROM faculties
+       DELETE FROM faculties
+    ''')
 
-''')
-    
 def add_group(name, pk):
     cursor.execute(f'''
-    INSERT INTO groups (name, faculty_id) VALUES 
-        ({name}, {pk})                      
-    ''')
+        INSERT INTO groups (name, faculty_id) VALUES 
+            ({name}, {pk})
+        ''')
 
 def get_faculty_by_name(name):
     cursor.execute(f'''
-        SELECT * FROM faculties WHERE name = "{name}" LIMIT 1                      
+        SELECT * FROM faculties WHERE name = "{name}" LIMIT 1
     ''')
 
     return cursor.fetchall()
 
 
 
-faculty = get_faculty_by_name('ФМФ')
-
-if faculty:
-    faculty = faculty[0]
-
 
 groups = [
-        {
-            'group_name': '423',
-            'faculty_name': 'ФМФ',
-        },
-         {
-            'group_name': '413',
-            'faculty_name': 'ФМФ',
-        },
-         {
-            'group_name': '403',
-            'faculty_name': 'ФМФ',
-        },
+    # {
+    #     'group_name': '423',
+    #     'faculty_name': 'ФМФ',
+    # },
+    # {
+    #     'group_name': '413',
+    #     'faculty_name': 'ФМФ',
+    # },
+    # {
+    #     'group_name': '403',
+    #     'faculty_name': 'ФМФ',
+    # },
+    {
+        'group_name': '412',
+        'faculty_name': 'ФМФ',
+    },
+]
 
-    ]
 def add_groups():
-    for item in groups():
+    for item in groups:
         faculty = get_faculty_by_name(item.get('faculty_name'))
         if not faculty:
             continue
         
         faculty = faculty[0]
 
-        
         add_group(item.get('group_name'), faculty[0])
-        
-        
+           
+# faculty = get_faculty_by_name('ФМФ')
 
-#add_group('423', faculty[0])
+# if faculty:
+#     faculty = faculty[0]
 
+# add_group('423', faculty[0])
+# print(faculty)
 
-#print(faculty)
+add_groups()
 
-#truncate_faculties()
-#add_faculties()
-
+# truncate_faculties()
+# add_faculties()
 
 connection.commit()
 connection.close()
